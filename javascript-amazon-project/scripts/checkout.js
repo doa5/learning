22e9f98fd0c26233985db5801ff99393c56d2958
doa5 <odiri101@gmail.com>
2025-12-09 08:42:36 +0000

    feat: load products from the backend

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,11 @@
 import { renderOrderSumamry } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js"
+import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js'
-import '../data/backend-practise.js'
+// import '../data/backend-practise.js'
 
-renderOrderSumamry()
-renderPaymentSummary()
+loadProducts(() => {
+  renderOrderSumamry()
+  renderPaymentSummary()
+})
+
