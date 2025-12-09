ab1fc4620a994dc0f2d5e9fa7b3ecdfeeaf3bbf2
doa5 <odiri101@gmail.com>
2025-12-09 09:12:44 +0000

    feat: learn promises and convert code to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,11 +1,54 @@
 import { renderOrderSumamry } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js"
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import '../data/backend-practise.js'
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1')
+    })
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve()
+    })
+  })
+
+]).then((values)=> {
+  console.log(values)
   renderOrderSumamry()
   renderPaymentSummary()
 })
 
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1')
+  })
+
+}).then((value) => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve()
+    })
+  })
+
+}).then(() => {
+  renderOrderSumamry()
+  renderPaymentSummary()
+})
+  */
+
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSumamry()
+    renderPaymentSummary()
+  })
+})
+*/
